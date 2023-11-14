2302847457723e3bfcd026ed7d306ae02333fe3e
Yu-Chen Lee <113709950+rennylee@users.noreply.github.com>
2023-11-13 21:51:18 -0500

    add serving and waiting time statics

--- MultipleQueueSystem.java
+++ MultipleQueueSystem.java
@@ -8,6 +8,7 @@ class MultipleQueueSystem {
     List<Queue<Patient>> queues;
     ServicePoint[] servicePoints;
     Random random;
+    List<Patient> servedPatients = new ArrayList<>();
 
     public MultipleQueueSystem(int queueCount, int servicePointCount) {
         queues = new ArrayList<>();
@@ -23,52 +24,57 @@ class MultipleQueueSystem {
 
     public void enqueuePatient(int queueIndex, Patient patient) {
         queues.get(queueIndex).add(patient);
-        System.out.println("Patient " + patient.id + " enqueued to Queue " + queueIndex);
+        System.out.println("Patient " + patient.id + " enqueued to Queue " + queueIndex +
+                " (Arrival Time: " + patient.arrivalTime + " seconds)");
     }
 
     public void simulateService() {
         int timeElapsed = 0;
         int maxSimulationDuration = 10000; // Set a maximum simulation duration in iterations
-    
+
         while (timeElapsed < maxSimulationDuration) {
             boolean allQueuesEmpty = true;
-    
+
             for (int i = 0; i < servicePoints.length; i++) {
                 if (!queues.get(i).isEmpty()) {
                     allQueuesEmpty = false;
                     Patient patient = queues.get(i).poll();
                     servicePoints[i].servePatient(patient);
+                    servedPatients.add(patient);
                 }
             }
-    
+
             if (allQueuesEmpty) {
                 System.out.println("Simulation ended: All queues are empty.");
                 break;
             }
-    
+
             try {
                 Thread.sleep(1000); // Simulating time passing in milliseconds
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-    
+
             timeElapsed++;
         }
     }
-    //doing distribution, but still testing
-    public void simulatePatientArrivals(int queueIndex, double lambda, int maxArrivalTime) {
-        int arrivalTime = 0;
-        int patient_id = 1;
-        while (arrivalTime < maxArrivalTime) {
-            Patient patient = new Patient(patient_id);
-            enqueuePatient(queueIndex, patient);
-            try {
-                Thread.sleep((long) (-Math.log(1 - random.nextDouble()) / lambda));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            arrivalTime += 1;
+
+    public void printStatistics() {
+        double totalWaitingTime = 0;
+        double totalServingTime = 0;
+
+        for (Patient patient : servedPatients) {
+            totalWaitingTime += patient.waitingTime;
+            totalServingTime += patient.serviceTime;
+            System.out.println("Patient " + patient.id + " - Waiting Time: " + patient.waitingTime + " seconds, Serving Time: " + patient.serviceTime + " seconds");
         }
+
+        double totalSystemTime = totalWaitingTime + totalServingTime;
+        double averageWaitingTime = totalWaitingTime / servedPatients.size();
+        double averageServingTime = totalServingTime / servedPatients.size();
+
+        System.out.println("Total System Time: " + totalSystemTime + " seconds");
+        System.out.println("Average Waiting Time: " + averageWaitingTime + " seconds");
+        System.out.println("Average Serving Time: " + averageServingTime + " seconds");
     }
-    
 }
