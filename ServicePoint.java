cede6a0ab6778f8be17b974820baaa1ee77229d7
Yu-Chen Lee <113709950+rennylee@users.noreply.github.com>
2023-11-13 21:52:06 -0500

    Update ServicePoint.java

--- ServicePoint.java
+++ ServicePoint.java
@@ -6,15 +6,18 @@ class ServicePoint {
     }
 
     public void servePatient(Patient patient) {
+        patient.startTime = System.currentTimeMillis() / 1000.0; // Record start time in seconds
         // Simulate serving the patient
         String time = String.format("%.3f", patient.serviceTime);
         System.out.println("Service Point " + id + " serving Patient " + patient.id +
-                " (Service Time: %.3f" + time + " seconds)");
+                " (Service Time: " + time + " seconds)");
         try {
             Thread.sleep((long)(patient.serviceTime * 1000)); // Simulating service time in seconds
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        patient.endTime = System.currentTimeMillis() / 1000.0; // Record end time in seconds
+        patient.waitingTime = patient.startTime - patient.arrivalTime;
         System.out.println("Service Point " + id + " finished serving Patient " + patient.id);
     }
 }
