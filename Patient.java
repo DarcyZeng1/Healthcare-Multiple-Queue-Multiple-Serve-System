15a75667e28690bade900600a3135fdb3b1c3bb9
Yu-Chen Lee <113709950+rennylee@users.noreply.github.com>
2023-11-13 21:51:49 -0500

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -3,12 +3,17 @@ import java.util.Random;
 class Patient {
     int id;
     double serviceTime;
+    double arrivalTime;
+    double startTime;
+    double endTime;
+    double waitingTime;
     double meanServiceTime = 20;
     double stdServiceTime = 5;
     Random random = new Random();
 
-    public Patient(int id) {
+    public Patient(int id, double arrivalTime) {
         this.id = id;
+        this.arrivalTime = arrivalTime;
         this.serviceTime = Math.max(0, meanServiceTime + random.nextGaussian() * stdServiceTime);
     }
 }
