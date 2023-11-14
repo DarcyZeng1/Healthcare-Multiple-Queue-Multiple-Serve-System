8724673b8c543cb69d22205eda93e166cdc7c09a
Yu-Chen Lee <113709950+rennylee@users.noreply.github.com>
2023-11-13 21:50:14 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import java.util.Random;
+iimport java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,14 +8,19 @@ public class Main {
         MultipleQueueSystem system = new MultipleQueueSystem(queueCount, servicePointCount);
 
         // Example: Enqueue 10 patients to different queues
+        double arrivalTime = 0;
         for (int i = 1; i <= 10; i++) {
             int queueIndex = new Random().nextInt(queueCount);
-            Patient patient = new Patient(i);
+            Patient patient = new Patient(i, arrivalTime);
+            arrivalTime += patient.serviceTime; // Adjust arrival time based on service time
             system.enqueuePatient(queueIndex, patient);
         }
 
         // Start the simulation
         system.simulateService();
+
+        // Calculate and print statistics
+        system.printStatistics();
 
         System.out.println("Simulation Complete");
     }
